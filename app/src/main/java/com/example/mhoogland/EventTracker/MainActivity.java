fa5598271a9ed5b78e75501f6bfbf27c580a6d03
M Hoogland <hmhdiswiss@hush.com>
2015-09-25 19:27:57 +0200

    Updated project.

--- app/src/main/java/com/example/mhoogland/EventTracker/MainActivity.java
+++ app/src/main/java/com/example/mhoogland/EventTracker/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.mhoogland.EventTracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
